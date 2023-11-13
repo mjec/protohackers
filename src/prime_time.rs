4d7efc66383697fe465f3605c1fe59d94280689f
Emjek Cordover <git@mjec.net>
2023-11-13 03:28:29 -0500

    Solve prime time

--- src/prime_time.rs
+++ src/prime_time.rs
@@ -1,11 +1,9 @@
-use serde::de::Visitor;
 use serde::{Deserialize, Serialize};
 
 use crate::{scaffolding::Context, server};
 use std::error::Error;
 use std::fmt::Display;
 use std::io::{ErrorKind, Read, Write};
-use std::marker::PhantomData;
 use std::net::{SocketAddr, TcpStream};
 
 #[derive(Debug)]
@@ -43,46 +41,11 @@ impl Display for Method {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
 enum Number {
     Integer(i64),
     Float(f64),
-}
-
-struct NumberVisitor<'a> {
-    marker: PhantomData<fn() -> &'a Number>,
-}
-impl NumberVisitor<'_> {
-    pub fn new() -> Self {
-        Self {
-            marker: PhantomData,
-        }
-    }
-}
-
-impl Visitor<'_> for NumberVisitor<'_> {
-    type Value = Number;
-
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        writeln!(formatter, "Expecting a number")
-    }
-
-    fn visit_i64<E>(self, value: i64) -> Result<Number, E> {
-        Ok(Number::Integer(value))
-    }
-
-    fn visit_f64<E>(self, value: f64) -> Result<Number, E> {
-        Ok(Number::Float(value))
-    }
-}
-
-impl<'a> Deserialize<'a> for Number {
-    fn deserialize<D>(deserializer: D) -> Result<Number, D::Error>
-    where
-        D: serde::Deserializer<'a>,
-    {
-        deserializer.deserialize_any(NumberVisitor::<'a>::new())
-    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -105,14 +68,68 @@ pub(crate) fn run(ctx: &Context) -> Result<(), Box<dyn Error>> {
 }
 
 fn handle(stream: &mut TcpStream, _remote_address: &SocketAddr) -> Result<(), Box<dyn Error>> {
-    let mut buffer = [0; 1024];
+    const BUFFER_SIZE: usize = 1024;
+    let mut buffer = [0; BUFFER_SIZE];
+    let mut remainder: Vec<u8> = Vec::with_capacity(BUFFER_SIZE);
     loop {
         match stream.read(&mut buffer) {
             Ok(0) => break Ok(()), // EOF,
-            Ok(bytes_read) => todo!(),
+            Ok(bytes_read) => {
+                let mut lines: Vec<&[u8]> = buffer[..bytes_read].split(|b| *b == b'\n').collect();
+                if let Some(unfinished_line) = lines.pop() {
+                    if lines.is_empty() {
+                        remainder.extend_from_slice(unfinished_line);
+                    } else {
+                        let mut old_remainder =
+                            std::mem::replace(&mut remainder, unfinished_line.into());
+                        old_remainder.extend_from_slice(lines[0]);
+                        lines[0] = &old_remainder;
+                        for line in lines {
+                            let maybe_request: Result<Request, serde_json::Error> =
+                                serde_json::from_slice(line);
+                            if let Ok(request) = maybe_request {
+                                let response = Response {
+                                    method: request.method,
+                                    prime: match request.number {
+                                        Number::Integer(n) => is_prime(n),
+                                        Number::Float(_) => false,
+                                    },
+                                };
+                                let mut response = serde_json::to_vec(&response)?;
+                                response.push(b'\n');
+                                stream.write_all(&response)?;
+                            } else {
+                                log::info!(
+                                    "Invalid request received: {:?}\n{:?}\n{:?}",
+                                    maybe_request.unwrap_err(),
+                                    &buffer[..bytes_read],
+                                    line
+                                );
+                                stream.write_all(b"kthxbai\n")?;
+                                return Ok(());
+                            }
+                        }
+                    }
+                }
+                Ok(())
+            }
             Err(e) if e.kind() == ErrorKind::Interrupted => Ok(()),
             Err(e) => Err(e),
         }?
+    }
+}
+
+fn is_prime(number: i64) -> bool {
+    if number < 2 {
+        false
+    } else if number == 2 {
+        true
+    } else if number % 2 == 0 {
+        false
+    } else {
+        !(3..=((number as f64).sqrt() as i64))
+            .step_by(2)
+            .any(|n| number % n == 0)
     }
 }
 
